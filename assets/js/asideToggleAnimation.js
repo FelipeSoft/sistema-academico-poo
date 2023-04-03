2685f203c5966d7b98046e5eb541518f89d4707b
FelipeSoft <felipedecastrolima2@gmail.com>
2023-04-03 09:57:20 -0300

    Estilização do aside e animações de collapse

---
+++ assets/js/asideToggleAnimation.js
@@ -0,0 +1,19 @@
+document.querySelector("div.section-item.s1").addEventListener("click", ()=>{
+    if(document.querySelector("div.toggle-area-1").style.display === 'block'){
+        document.querySelector("div.toggle-area-1").style.display = 'none';
+        document.querySelector("div.section-item.s1 i").classList = 'fa fa-caret-down';
+    } else {
+        document.querySelector("div.toggle-area-1").style.display = 'block';
+        document.querySelector("div.section-item.s1 i").classList = 'fa fa-caret-up';
+    }
+});
+
+document.querySelector("div.section-item.s2").addEventListener("click", ()=>{
+    if(document.querySelector("div.toggle-area-2").style.display === 'block'){
+        document.querySelector("div.toggle-area-2").style.display = 'none';
+        document.querySelector("div.section-item.s2 i").classList = 'fa fa-caret-down';
+    } else {
+        document.querySelector("div.toggle-area-2").style.display = 'block';
+        document.querySelector("div.section-item.s2 i").classList = 'fa fa-caret-up';
+    }
+});
--- index.php
+++ index.php
@@ -16,16 +16,45 @@ if(!isset($_SESSION['logged_person'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Início</title>
     <link rel="stylesheet" href="<?=$base_url?>/assets/css/home.css">
+    <script src="https://use.fontawesome.com/a443ad607e.js"></script>
 </head>
 <body>
-    <p>
+    <!-- <p>
         <?php
-            if(isset($_SESSION['logged_person'])){
-                echo $_SESSION['logged_person'];
-            }
+            //if(isset($_SESSION['logged_person'])){
+            //    echo $_SESSION['logged_person'];
+            //}
         ?>
         -
         <a href="<?=$base_url?>/actions/logout_action.php">Sair</a>
-    </p>
+    </p> -->
+    <header>
+        <div class="max-width">
+            <h3>SISTEMA ACADÊMICO</h3>
+        </div>
+    </header>
+
+    <main>
+        <aside>
+            <div class="sections">
+                <div class="section-item s1">Resultados <i class="fa fa-caret-down"></i></div>
+                <div class="toggle-area-1">
+                    <div class="toggle-item"><a href="">Notas Parcias</a></div>
+                    <div class="toggle-item"><a href="">Notas Finais</a></div>
+                </div>
+                <div class="section-item s2">Aluno <i class="fa fa-caret-down"></i></div>
+                <div class="toggle-area-2">
+                    <div class="toggle-item"><a href="">Aulas e Conteúdos</a></div>
+                    <div class="toggle-item"><a href="">Calendário</a></div>
+                    <div class="toggle-item"><a href="">Comunicados</a></div>
+                    <div class="toggle-item"><a href="">Mensagens</a></div>
+                    <div class="toggle-item"><a href="">Quadro de Horários</a></div>
+                    <div class="toggle-item"><a href="">Atividades e Avaliações</a></div>
+                </div>
+                <div class="section-item s3"><a href="">Dashboard</a></div>
+            </div>
+        </aside>
+    </main>
+    <script src="<?=$base_url?>/assets/js/asideToggleAnimation.js"></script>
 </body>
 </html>
