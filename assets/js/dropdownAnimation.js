a36312a9d1a2eacfa5a772cf9cfe32edf0b81ba4
FelipeSoft <felipedecastrolima2@gmail.com>
2023-04-06 16:35:59 -0300

    Javascript marginLeft

--- assets/js/dropdownAnimation.js
+++ assets/js/dropdownAnimation.js
@@ -1,6 +1,6 @@
 document.querySelector("div.profile").addEventListener("click", ()=>{
     if(document.querySelector("div.profile-dropdown").style.marginLeft === '0px'){
-        document.querySelector("div.profile-dropdown").style.marginLeft = '250px';
+        document.querySelector("div.profile-dropdown").style.marginLeft = '400px';
     } else {
         document.querySelector("div.profile-dropdown").style.marginLeft = '0px';
     }
