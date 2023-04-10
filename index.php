e4e1040ffedffd5e6f33d5c0283a2edac4d7e105
FelipeSoft <felipedecastrolima2@gmail.com>
2023-04-10 15:21:41 -0300

    Estilização das páginas index.php e student.php

--- index.php
+++ index.php
@@ -22,6 +22,66 @@ if(!isset($_SESSION['logged_user'])){
             }
         }
     ?>
+    <div class="container container-home">
+        <div class="home-buttons">
+            <div class="home-option">
+                Transações
+            </div>
+            <div class="home-option">
+                Salários
+            </div>
+            <div class="home-option">
+                Comunicados
+            </div>
+            <div class="home-option">
+                Dashboard
+            </div>
+        </div>
+        <div class="average-graphic">
+            Área do Gráfico das Médias
+        </div>
+        <div class="courses">
+        <h3>Nossos Cursos</h3>
+            <div class="container-courses">
+                <div class="courses-item">
+                    Item 1
+                </div>
+                <div class="courses-item">
+                    Item 2
+                </div>
+                <div class="courses-item">
+                    Item 3
+                </div>
+                <div class="courses-item">
+                    Item 4
+                </div>
+                <div class="courses-item">
+                    Item 1
+                </div>
+                <div class="courses-item">
+                    Item 2
+                </div>
+                <div class="courses-item">
+                    Item 3
+                </div>
+                <div class="courses-item">
+                    Item 4
+                </div>
+                <div class="courses-item">
+                    Item 1
+                </div>
+                <div class="courses-item">
+                    Item 2
+                </div>
+                <div class="courses-item">
+                    Item 3
+                </div>
+                <div class="courses-item">
+                    Item 4
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php require("C:/xampp/htdocs/sistema-academico-poo/components/footer.php") ?>
--- student.php
+++ student.php
@@ -7,75 +7,84 @@ require("C:/xampp/htdocs/sistema-academico-poo/components/header.php");
 <main>
     <?php require("C:/xampp/htdocs/sistema-academico-poo/components/asideAdministrator.php") ?>
     <div class="container">
-        <h3>Novo Cadastro</h3>
+        <h2 class="main-title">Novo Cadastro</h2>
         <form action="<?=$base_url?>/actions/student_action.php" method="POST">
-            <p>Área do Estudante</p>
-    
-            <div class="student-area">
-                <div class="personal-info">  
-                    <label>
-                    Nome:
-                    <input type="text">
-                    </label>
-                    <label>
-                        Data de Nascimento:
-                        <input type="date">
-                    </label>
-                    <label>
-                        RG:
+                <h3>Área do Estudante</h3>
+                <div class="student-area">
+                    <div class="column-left-sa">
+                        <label>
+                        Nome:
                         <input type="text">
-                    </label>
-                    <label>
-                        CPF:
-                        <input type="text">
-                    </label>
+                        </label>
+                        <label>
+                            Data de Nascimento:
+                            <input type="date">
+                        </label>
+                        <label>
+                            RG:
+                            <input type="text">
+                        </label>
+                        <label>
+                            CPF:
+                            <input type="text">
+                        </label>
+                    </div>
+                    <div class="column-right-sa">
+                        <label>
+                            Escolaridade:
+                            <select name="schooling">
+                                <option value="1">Ensino Fundamental I</option>
+                                <option value="2">Ensino Fundamental II</option>
+                                <option value="3">Ensino Médio</option>
+                                <option value="4">Ensino Superior</option>
+                            </select>
+                        </label>    
+                        <label>
+                            Série/Turma:
+                            <input type="text">
+                        </label>
+                    </div>
                 </div>
-        
-                <div class="schooling-info">
-                    <label>
-                        Série/Turma:
-                        <input type="text">
-                    </label>
-                    <label>
-                        Escolaridade:
-                        <input type="text">
-                    </label>
-                </div>  
-            </div>
-
-            <p>Área do Responsável</p>
-            <div class="guardian-area">
-                <label>
-                    Nome:
-                <input type="text">
-                </label>
-                <label>
-                    Email:
-                    <input type="text">
-                </label>
-                <label>
-                    Data de Nascimento:
-                    <input type="date">
-                </label>
-                <label>
-                    RG:
-                    <input type="text">
-                </label>
-                <label>
-                    CPF:
-                    <input type="text">
-                </label>
-                <label>
-                    Telefone:
-                    <input type="text">
-                </label>
-                <label>
-                    Celular:
-                    <input type="text">
-                </label>
-            </div>
-            <button>Cadastrar</button>
-        </form>
+    
+                <h3>Área do Responsável</h3>
+                    <div class="guardian-area">
+                        <div class="column-left-ga">
+                            <label>
+                            Nome:
+                            <input type="text">
+                            </label>
+                            <label>
+                                Email:
+                                <input type="text">
+                            </label>
+                            <label>
+                                Data de Nascimento:
+                                <input type="date">
+                            </label>
+                            <label>
+                                RG:
+                                <input type="text">
+                            </label>
+                            <label>
+                                CPF:
+                                <input type="text">
+                            </label>
+                        </div>
+                        <div class="column-right-ga">
+                            <label>
+                            Telefone:
+                            <input type="text">
+                            </label>
+                            <label>
+                                Celular:
+                                <input type="text">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="button-area">
+                    <button>Cadastrar</button>
+                    </div>
+                </form>
     </div>
 </main>
 
